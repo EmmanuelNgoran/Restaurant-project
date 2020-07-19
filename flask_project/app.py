13470f203d8f50e3fb49f0ec2fd25189b9300468
Emmanuel Ngoran <cedricngoran1997@gmail.com>
2020-07-19 03:41:55 -0400

    done

--- flask_project/app.py
+++ flask_project/app.py
@@ -2,6 +2,9 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
+db = SQLAlchemy(app)
+
 
 @app.route('/')
 def hello_world():
