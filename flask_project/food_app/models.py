727f17c7db400084b8f6195558f9217f98ff3f25
Emmanuel Ngoran <cedricngoran1997@gmail.com>
2020-08-10 17:39:04 -0400

    -Modify the model for the database -add exception in the gitignore file

--- flask_project/food_app/models.py
+++ flask_project/food_app/models.py
@@ -7,9 +7,7 @@ class Restaurant(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(40), nullable=False)
     description = db.Column(db.String(200), nullable=True)
-    address = db.Column(db.String(100) , nullable=True)
-    city = db.Column(db.String(35) , nullable=False)
-    country = db.Column(db.String(35) , nullable=False)
+    address = db.Column(db.Integer , db.ForeignKey('address.id') , nullable=True)
     telephone_number = db.Column(db.String(10) , nullable=False)
     average_price=db.Column(db.Integer , nullable=True)
     date_created = db.Column(db.DateTime, default=datetime.utcnow)
@@ -17,11 +15,9 @@ class Restaurant(db.Model):
     
     dishes = db.relationship('Dish', backref='resto', lazy=True)
     menus = db.relationship('Menu', backref='resto', lazy=True)
-    user = db.relationship('User', backref='resto' , lazy=True)
-    address = db.relationship('User', backref='resto' , lazy=True)
-
+    
     def __repr__(self):
-        return f"Restaurant('{self.name}', '{self.city}')"
+        return f"<Restaurant('{self.name}', '{self.city}')>"
 
 
 class Address(db.Model):
@@ -30,6 +26,8 @@ class Address(db.Model):
     city = db.Column(db.String(50), nullable=False)
     state_id = db.Column(db.Integer ,db.ForeignKey('state.id') , nullable=True )
     country_id = db.Column(db.Integer ,db.ForeignKey('country.id') , nullable=True )
+    
+    
 
     def __repr__(self):
         return f"<Address('{self.street_address}')>'"
@@ -39,12 +37,19 @@ class State(db.Model):
     name = db.Column(db.String(50) , nullable=False)
     country_id = db.Column(db.Integer ,db.ForeignKey('country.id') , nullable=True )
 
+    def __repr__(self):
+        return f"<State('{self.name}')>'"
+
 class Country(db.Model):
     id=db.Column(db.Integer , primary_key=True)
     name=db.Column(db.String(50),nullable=False)
     code=db.Column(db.Integer , nullable=True)
 
     address = db.relationship('Restaurant', backref='country', lazy=True)
+
+    def __repr__(self):
+        return f"<Country('{self.name}')>'"
+
 class Menu(db.Model):
     id= db.Column(db.Integer , primary_key=True)
     name = db.Column(db.String(30), nullable=False)
@@ -53,7 +58,7 @@ class Menu(db.Model):
     restaurant_id = db.Column(db.Integer, db.ForeignKey('restaurant.id'), nullable=False)
 
     def __repr__(self):
-        return f"Menu('{self.name}')"
+        return f"<Menu('{self.name}')>"
     
 
 class Dish(db.Model):
@@ -65,19 +70,31 @@ class Dish(db.Model):
     restaurant_id = db.Column(db.Integer, db.ForeignKey('restaurant.id'), nullable=False)
 
     def __repr__(self):
-        return f"Restaurant('{self.name}', '{self.price}')"
+        return f"<Dish('{self.name}', '{self.price}')>"
 
 class Speciality(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(40), nullable=False)
      
     def __repr__(self):
-        return f"Speciality('{self.name}')"
+        return f"<Speciality('{self.name}')>"
 
 class Resto_speciality(db.Model):
     id = db.Column(db.Integer , primary_key=True)
     restaurant_id = db.Column(db.Integer, db.ForeignKey('restaurant.id'), nullable=False)
     speciality_id = db.Column(db.Integer, db.ForeignKey('speciality.id'), nullable=False)
+
+    def __repr__(self):
+        return f"<Resto_speciality('{self.restaurant_id}')>"
+
+
+class ImageItem(db.Model):
+    id = db.Column(db.Integer , primary_key=True)
+    photo_path=db.Column(db.String(100) , unique=True , nullable=False)
+    restaurant_pk = db.Column(db.Integer , db.ForeignKey('restaurant.id') , nullable=True)
+
+    def __repr__(self):
+        return f"<ImageItem('{self.photo_path}')>"
 
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -87,15 +104,17 @@ class User(db.Model):
     password = db.Column(db.String(60), nullable=False)
     date_created = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
     
+    resto = db.relationship('Restaurant', backref='user' , lazy=True)
+
     def __repr__(self):
-        return f"User('{self.username}', '{self.email}', '{self.image_file}')"
+        return f"<User('{self.username}', '{self.email}', '{self.image_file}')>"
 
 
 class RestaurantOwner(User):
     restaurant_id = db.Column(db.Integer,db.ForeignKey('restaurant.id'), nullable=False)
 
     def __repr__(self):
-        return f"RestaurantOwner('{self.username}','{self.email}')"
+        return f"<RestaurantOwner('{self.username}','{self.email}')>"
 
 class Review(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -103,5 +122,6 @@ class Review(db.Model):
     content = db.Column(db.Text, nullable=False)
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
     restaurant_id = db.Column(db.Integer, db.ForeignKey('restaurant.id'), nullable=False)
+
     def __repr__(self):
-        return f"Review('{self.user_id}', '{self.date_posted}')"
+        return f"<Review('{self.user_id}', '{self.date_posted}')>"
